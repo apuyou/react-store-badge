54acbe1dd987cdf7750ef33a259cfd7e001da34c
Arthur Puyou <arthur@puyou.fr>
2019-12-14 21:38:55 +0100

    Add Babel and component code

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
 import pkg from './package.json';
+import babel from 'rollup-plugin-babel';
+import image from '@rollup/plugin-image';
 
 export default [
   {
@@ -6,6 +8,13 @@ export default [
     output: [
       { file: pkg.main, format: 'cjs' },
       { file: pkg.module, format: 'es' },
+    ],
+    external: ['react'],
+    plugins: [
+      babel({
+        exclude: 'node_modules/**',
+      }),
+      image(),
     ],
   },
 ];
